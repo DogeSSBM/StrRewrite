069084d4940d9ed8ece1f33485d5a31b338e8745
dogeThinkpad <dogessbm@gmail.com>
2021-09-12 00:10:21 -0400

    Automatic commit.

--- main.c
+++ main.c
@@ -1,17 +1,22 @@
 #include "Includes.h"
 
-typedef enum{K_NONE = 0, K_CB_O, K_CB_C, K_SB_O, K_SB_C, K_STR, K_VAR, K_SARR, K_DARR}TokenType;
+typedef enum{T_STR, T_VAR}TermType;
 
-typedef struct Token_s{
-    struct Token_s *prev;
-    TokenType type;
-    char *start;
-    char *end;
-    uint len;
-    struct Token_s *next;
-}Token;
+typedef struct Term_s{
+    TermType type;
+    union{
+        struct{
+            
+        };
+    }
+}Term;
 
-char* readFile(char *filePath)
+typedef struct Rule_s{
+
+}Rule;
+
+char *readFile
+(char *filePath)
 {
     char *buffer = 0;
     long length;
@@ -29,37 +34,6 @@ char* readFile(char *filePath)
     buffer[length] = '\0';
     fclose(f);
     return buffer;
-}
-
-
-Token *nextToken
-(Token *current)
-{
-    current->next = calloc(1, sizeof(Token));
-    current->next->prev = current;
-    return current->next;
-}
-
-Token *tokenize
-(char *text)
-{
-    Token *list = calloc(1, sizeof(Token));
-    char *pos = text;
-    while(1){
-        char c = *pos;
-        switch(c){
-            case ' ':
-            case '\t':
-            case '\n':
-                pos++;
-                break;
-            case '{':
-                list->type = K_CB_O;
-                list->start = pos;
-                list->end = pos+1;
-                list->len = 1;
-        }
-    }
 }
 
 int main
