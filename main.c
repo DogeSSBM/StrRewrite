a98a9ae36e5381a1b0dd3077a4c1e02ab1407e29
DogeSSBM <dogessbm@gmail.com>
2021-09-13 07:12:07 -0400

    Automatic commit.

--- main.c
+++ main.c
@@ -1,19 +1,25 @@
 #include "Includes.h"
-
+#define err()   {printf("Err at line %i\n", __LINE__)}
 typedef enum{T_STR, T_VAR}TermType;
 
 typedef struct Term_s{
     TermType type;
-    union{
-        struct{
-            
-        };
-    }
+    char *name; // NULL if T_STR.
+    char *text; // text of string if T_STR. text of T_VAR if bound. NULL if free.
+    struct Term_s *next;
 }Term;
 
 typedef struct Rule_s{
-
+    Term *l;
+    Term *r;
+    struct Rule_s *next;
 }Rule;
+
+typedef struct RuleSetSeq_s{
+    char *name;
+    Rule *rules;
+    struct RuleSetSeq_s *next;
+}RuleSetSeq;
 
 char *readFile
 (char *filePath)
@@ -36,10 +42,62 @@ char *readFile
     return buffer;
 }
 
+uint strCount
+(char *source, char *str)
+{
+    uint count = 0;
+    while((source = strstr(source, str)) != NULL){
+        count++;
+        source++;
+    }
+    // printf("count: %u\n", count);
+    return count;
+}
+
+char *getStrN
+(char *source, char *str, const uint n)
+{
+    uint count = 0;
+    while((source = strstr(source, str)) != NULL && count < n){
+        count++;
+        source++;
+    }
+    return source == NULL ? NULL : source-1;
+}
+
+RuleSetSeq *parseSeq
+(char *source)
+{
+    const uint count = strCount(source, "@");
+    if(count == 0)
+        return NULL;
+    RuleSetSeq *seq = calloc(1, sizeof(RuleSetSeq));
+    RuleSetSeq *current = seq;
+    for(uint i = 0; i < count; i++){
+        printf("%u/%u\n", i, count);
+        char *pos = getStrN(source, "@", i)+2;
+        printf("%c\n", *pos);
+        char *end = strpbrk(pos, " \t\n");
+
+        uint len = end-pos;
+        printf("len: %u\n", len);
+
+        current->name = calloc(len+1, sizeof(char));
+        memcpy(current->name, pos, len);
+        current->name[len] = '\0';
+        printf("parsing rule seq %u \"%s\"\n", i, current->name);
+        if(i+1<count)
+            current->next = calloc(1, sizeof(RuleSetSeq));
+        current = current->next;
+    }
+    printf("done\n");
+    return seq;
+}
+
 int main
 (int argc, char **argv)
 {
     char *text = readFile(argc == 2? argv[1] : "./Test.txt");
-
+    parseSeq(text);
     return 0;
 }
