7e89d3eed097a496eff62aad119d10aec717b315
DogeSSBM <dogessbm@gmail.com>
2021-09-13 20:30:04 -0400

    Automatic commit.

--- main.c
+++ main.c
@@ -15,11 +15,23 @@ typedef struct Rule_s{
     struct Rule_s *next;
 }Rule;
 
-typedef struct RuleSetSeq_s{
+typedef struct RuleSet_s{
     char *name;
     Rule *rules;
-    struct RuleSetSeq_s *next;
-}RuleSetSeq;
+    struct RuleSet_s *next;
+}RuleSet;
+
+typedef struct Sym_s{
+    SymType type;
+    char *start;
+    char *stop;
+    union{
+        Term *term;
+        Rule *rule;
+        RuleSet *ruleset;
+    }
+    struct Sym_s *next;
+}Sym;
 
 char *readFile
 (char *filePath)
@@ -42,211 +54,13 @@ char *readFile
     return buffer;
 }
 
-uint strCount
-(char *source, char *str)
-{
-    uint count = 0;
-    while((source = strstr(source, str)) != NULL){
-        count++;
-        source++;
-    }
-    // printf("count: %u\n", count);
-    return count;
-}
-
-char *getStrN
-(char *source, char *str, const uint n)
-{
-    uint count = 0;
-    while((source = strstr(source, str)) != NULL && count < n){
-        count++;
-        source++;
-    }
-    return source == NULL ? NULL : source-1;
-}
-
-char *getNextArrowBeforeEnd
-(char *pos, char *end)
-{
-    char *sArrowStart = strstr(pos, "->");
-    if(sArrowStart >= end || sArrowStart == NULL)
-        return NULL;
-    char *dArrowStart = strstr(pos, "<->");
-    return dArrowStart < sArrowStart? dArrowStart : sArrowStart;
-}
-
-char *getSeqEnd
-(char *seqStart)
-{
-    char *seqEnd = strstr(seqStart+1, "@");
-    return seqEnd == NULL ? seqStart+strlen(seqStart) : seqEnd;
-}
-
-char *getStartOfLine
-(char *pos)
-{
-    while(*pos != '\n')
-        pos--;
-    return pos+1;
-}
-
-char *getStartOfNextLine
-(char *pos)
-{
-    while(*pos != '\n')
-        pos++;
-    return pos+1;
-}
-
-uint getTermLen
-(char *pos)
-{
-    char *end = strpbrk(pos, "$_<\n-");
-    if(end == NULL)
-        err();
-    return end-pos;
-}
-
-char *getTermEnd
-(char *pos)
-{
-    pos += *pos=='$' || *pos == '_';
-    char *end = strpbrk(pos, "$_<\n-");
-    if(end == NULL)
-        err();
-    return end;
-}
-
-Term *parseTerm
-(char *pos)
-{
-    TermType t;
-    switch(*pos){
-        case '$':
-            t = T_STR;
-            break;
-        case '_':
-            t = T_VAR;
-            break;
-        case '-':
-        case '<':
-        case '\n':
-            return NULL;
-        default:
-            err();
-    }
-    pos++;
-    uint len = getTermLen(pos);
-    Term *term = calloc(1, sizeof(Term));
-    term->type = t;
-    if(t == T_STR){
-        term->text = calloc(len+1, sizeof(char));
-        memcpy(term->text, pos, len);
-        printf("$%s", term->text);
-    }else{
-        term->name = calloc(len+1, sizeof(char));
-        memcpy(term->name, pos, len);
-        printf("_%s", term->name);
-    }
-    return term;
-}
-
-Rule *parseRule
-(char *pos, char *end)
-{
-    char *arrowStart = getNextArrowBeforeEnd(pos, end);
-    if(arrowStart == NULL)
-        return NULL;
-    char *arrowEnd = strchr(arrowStart, '>');
-    if(*arrowEnd != '>' || arrowEnd != arrowStart+(*arrowStart == '<' ? 2 : 1))
-        err();
-    char *rStart = arrowEnd+1;
-    char *lStart = getStartOfLine(arrowStart);
-
-    Rule *rule = calloc(1, sizeof(Rule));
-    pos = lStart;
-    rule->l = parseTerm(pos);
-    Term *term = rule->l;
-    pos=getTermEnd(pos);
-    while((term->next = parseTerm(pos))!=NULL){
-        pos=getTermEnd(pos);
-        term = term->next;
-    }
-    printf("%s", *arrowStart == '<' ? "<->" : "->");
-    pos = rStart;
-    rule->r = parseTerm(pos);
-    term = rule->r;
-    pos = getTermEnd(pos);
-    while((term->next = parseTerm(pos))!=NULL){
-        pos=getTermEnd(pos);
-        term = term->next;
-    }
-    printf("\n");
-    return rule;
-}
-
-Rule *parseRuleSeq
-(char *pos, char *end)
-{
-    Rule *rules = calloc(1, sizeof(Rule));
-    Rule *current = rules;
-    current = parseRule(pos, end);
-    pos = getStartOfNextLine(pos);
-    while((current->next = parseRule(pos, end))!=NULL){
-        pos = getStartOfNextLine(pos);
-        current = current->next;
-    }
-    return rules;
-}
-
-RuleSetSeq *parseSeq
-(char *source)
-{
-    const uint count = strCount(source, "@");
-    if(count == 0)
-        return NULL;
-    RuleSetSeq *seq = calloc(1, sizeof(RuleSetSeq));
-    RuleSetSeq *current = seq;
-
-    for(uint i = 0; i < count; i++){
-        char *pos = getStrN(source, "@", i)+2;
-        char *end = strpbrk(pos, " \t\n");
-        uint len = end-pos;
-
-        current->name = calloc(len+1, sizeof(char));
-        memcpy(current->name, pos, len);
-        current->name[len] = '\0';
-
-        printf("Parsing rule sequence \"@%s\"\n", current->name);
-        end = getSeqEnd(pos);
-        current->rules = parseRuleSeq(getStartOfNextLine(pos), end);
-
-        // Rule *currentRule = NULL;
-        // Rule *nextRule = NULL;
-        // end = getSeqEnd(pos);
-        // char *rulePos = getStartOfLine(getNextArrowBeforeEnd(pos, end));
-        // while((nextRule = parseRule(rulePos))!=NULL){
-        //     if(current->rules == NULL){
-        //         currentRule = nextRule;
-        //         current->rules = currentRule;
-        //     }else{
-        //         currentRule->next = nextRule;
-        //         currentRule = currentRule->next;
-        //     }
-        //     rulePos = getStartOfNextLine(rulePos);
-        // }
-        if(i+1<count)
-            current->next = calloc(1, sizeof(RuleSetSeq));
-        current = current->next;
-    }
-    printf("done\n");
-    return seq;
-}
+Sym *appendSym
+(Sym)
 
 int main
 (int argc, char **argv)
 {
-    char *text = readFile(argc == 2? argv[1] : "./Test.txt");
-    parseSeq(text);
+    char *source = readFile(argc == 2? argv[1] : "./Test.txt");
+
     return 0;
 }
